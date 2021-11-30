a3859d62dd096389c4c5ea6d16ce8b117b7890d4
Abdullah <mmunna864@gmail.com>
2021-11-30 19:35:03 +0600

    init

---
+++ app/src/main/java/com/example/bootcampandroid/LoginActivity.java
@@ -0,0 +1,15 @@
+package com.example.bootcampandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
---
+++ app/src/main/java/com/example/bootcampandroid/authentication/LoginActivity.java
@@ -0,0 +1,16 @@
+package com.example.bootcampandroid.authentication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.bootcampandroid.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login2);
+    }
+}
